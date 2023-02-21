81d74e98c5f168050d621a05762d39e2cb951450
Bartek Łuka <bartoszluka1@gmail.com>
2023-02-21 22:51:41 +0100

    Add generic print_day function and day 2 part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -1,5 +1,4 @@
 use anyhow::{anyhow, Result};
-use std::num::ParseIntError;
 
 #[derive(Clone, Copy, Debug)]
 enum Rps {
@@ -27,6 +26,7 @@ impl Rps {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 enum GameResult {
     Win,
     Draw,
@@ -40,23 +40,24 @@ impl GameResult {
             Loss => 0,
         }
     }
+
+    fn from_string(string: &str) -> Result<Self> {
+        match string {
+            "X" => Ok(Loss),
+            "Y" => Ok(Draw),
+            "Z" => Ok(Win),
+            _ => Err(anyhow!(format!("invalid string '{string}'"))),
+        }
+    }
 }
 use GameResult::*;
 
-// fn to_pair<T: Copy>(list: impl Iterator<Item = T>) -> Result<(T, T)> {
-//     match list.collect::<Vec<T>>()[..] {
-//         [first, second, ..] => Ok((first, second)),
-//         _ => Err(anyhow!("list does not have exactly 2 elements")),
-//     }
-// }
-
-fn to_pair(vec: Vec<Rps>) -> Result<(Rps, Rps)> {
+fn to_pair<T: Copy>(vec: Vec<T>) -> Result<(T, T)> {
     match &vec[..] {
         &[first, second, ..] => Ok((first, second)),
         _ => Err(anyhow!("list does not have exactly 2 elements")),
     }
 }
-
 
 fn game_result(opponent: &Rps, player: &Rps) -> GameResult {
     match (opponent, player) {
@@ -88,5 +89,32 @@ pub fn part1(input: &String) -> Result<i32> {
 }
 
 pub fn part2(input: &String) -> Result<i32> {
-    Ok(0)
+    let rounds = input
+        .trim_end()
+        .split("\n")
+        .map(|line| line.split(" ").collect::<Vec<_>>())
+        .map(|vec| to_pair(vec))
+        .collect::<Result<Vec<_>>>()?;
+    let points = rounds
+        .iter()
+        .map(|(first, second)| {
+            let opponent = Rps::from_string(first)?;
+            let game_result = GameResult::from_string(second)?;
+            let player = match_result(opponent, game_result);
+            Ok(player.get_points() + game_result.get_points())
+        })
+        .collect::<Result<Vec<_>>>()?;
+    Ok(points.iter().sum())
+}
+
+fn match_result(opponent: Rps, game_result: GameResult) -> Rps {
+    match (opponent, game_result) {
+        (Rock, Win) => Paper,
+        (Rock, Loss) => Scissors,
+        (Paper, Win) => Scissors,
+        (Paper, Loss) => Rock,
+        (Scissors, Win) => Rock,
+        (Scissors, Loss) => Paper,
+        (any, Draw) => any,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,17 @@ mod days;
 use days::*;
 
 fn main() -> Result<()> {
-    print_day(1, day01::part1, day01::part2)?;
-    print_day(2, day02::part1, day02::part2)?;
+    print_day(1, day01::part1, 74711, day01::part2, 209481)?;
+    print_day(2, day02::part1, 9241, day02::part2, 14610)?;
     Ok(())
 }
 
 fn print_day(
     day: i32,
     part1: fn(&String) -> Result<i32>,
+    part1_expected: i32,
     part2: fn(&String) -> Result<i32>,
+    part2_expected: i32,
 ) -> Result<()> {
     let formatted_day = if day < 10 {
         format!("0{day}")
@@ -20,10 +22,18 @@ fn print_day(
         day.to_string()
     };
     let input = read_to_string(format!("inputs/day{formatted_day}")).map_err(|err| anyhow!(err))?;
-    let day01_part1 = part1(&input)?;
-    let day01_part2 = part2(&input)?;
-    println!("day 1");
-    println!("  part 1: {:?}", day01_part1);
-    println!("  part 2: {:?}", day01_part2);
+    let part1_result = part1(&input)?;
+    let part2_result = part2(&input)?;
+    println!("day {day}");
+    println!("  part 1: {}", compare(part1_result, part1_expected));
+    println!("  part 2: {}", compare(part2_result, part2_expected));
     Ok(())
+}
+
+fn compare<T: PartialEq + std::fmt::Debug>(got: T, expected: T) -> String {
+    if got == expected {
+        format!("✅ {:?}", got)
+    } else {
+        format!("🟥 got {:?} expected {:?}", got, expected)
+    }
 }
